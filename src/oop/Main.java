ec3fa83b2ffc868f6f70e0cbe7ca1cfd3d3d42cd
Yevgen <zhmurko.evgen@gmail.com>
2020-09-20 11:16:04 +0300

    test24

--- src/oop/Main.java
+++ src/oop/Main.java
@@ -5,8 +5,20 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         System.out.println("HI!");
+        System.out.println("HI2!");
+        System.out.println("HI2!");
+        System.out.println("HI2!");
+        System.out.println("HI2!");
+        System.out.println("HI3!");
+        System.out.println("HI4!");
+        System.out.println("HI5!");
+        System.out.println("HI5!");
+        System.out.println("HI5!");
+        System.out.println("HI5!");
+
         System.out.println("HI!");
         System.out.println("HI!123444");
         System.out.println("HI!123444");
+        System.out.println("Test25");
     }
 }
