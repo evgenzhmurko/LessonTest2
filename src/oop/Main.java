2dac41321d4a0b9e17c8fa416ff48a7a8016459c
Yevgen <zhmurko.evgen@gmail.com>
2020-09-20 10:10:50 +0300

    Commit message 3

--- src/oop/Main.java
+++ src/oop/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         System.out.println("HI!");
-        System.out.println("HI2!");
+        System.out.println("HI!");
+        System.out.println("HI!123444");
+        System.out.println("HI!123444");
     }
 }
