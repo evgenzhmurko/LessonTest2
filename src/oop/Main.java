0290712abd1789ad8935001db4854ef91bbbbd7c
Yevgen <zhmurko.evgen@gmail.com>
2020-09-20 10:45:19 +0300

    test24

--- src/oop/Main.java
+++ src/oop/Main.java
@@ -6,5 +6,8 @@ public class Main {
         // write your code here
         System.out.println("HI!");
         System.out.println("HI2!");
+        System.out.println("HI2!");
+        System.out.println("HI2!");
+        System.out.println("HI2!");
     }
 }
